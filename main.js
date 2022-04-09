046235b6cf9df4a049da6b0d7022e16d4e2cdb28
ankitha <94835759+ankimass@users.noreply.github.com>
2022-04-09 09:38:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
-var i = 0;
-function update()
-{
-    i++;
-    var numbers_of_family_member_in_array = 5
-    if(i > numbers_of_family_member_in_array)
-      {
-          i = 0;
-      }
-    
-    //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage = Image[i];
-    //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = names[i] ;
- 
-    document.getElementById("family_member_image").src = updatedImage;
-    document.getElementById("family_member_name").innerHTML = updatedName;
-}
+canvas=document.getElementById("mycanvas");
+
+ctx=canvas.getContext("2d");
+color="grey";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=1;
+ctx.rect(150,143,430,200,);
+ctx.stroke();
+
+ctx=canvas.getContext("2d");
+color="blue";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=5;
+ctx.arc(250,210,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx=canvas.getContext("2d");
+color="black";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=5;
+ctx.arc(350,210,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx=canvas.getContext("2d");
+color="red";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=5;
+ctx.arc(450,210,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx=canvas.getContext("2d");
+color="yellow";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=5;
+ctx.arc(300,250,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx=canvas.getContext("2d");
+color="green";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=5;
+ctx.arc(400,250,40,0,2*Math.PI);
+ctx.stroke();
