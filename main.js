f4dddab64026d1f3319973b46e82ae4f223c7d8b
ankitha <94835759+ankimass@users.noreply.github.com>
2022-04-09 09:54:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,49 @@
-canvas=document.getElementById("mycanvas");
+var names_of_people = [];
+    
+function submit()
+{
+    var GuestName = document.getElementById("name1").value;
+	names_of_people.push(GuestName);
+	console.log(GuestName);    
+    console.log(names_of_people);
+    var lenght_of_name = names_of_people.length;
+    console.log(lenght_of_name);
+	document.getElementById("display_name").innerHTML=names_of_people.toString();
+   }
 
-ctx=canvas.getContext("2d");
-color="grey";
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=1;
-ctx.rect(150,143,430,200,);
-ctx.stroke();
 
-ctx=canvas.getContext("2d");
-color="blue";
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=5;
-ctx.arc(250,210,40,0,2*Math.PI);
-ctx.stroke();
 
-ctx=canvas.getContext("2d");
-color="black";
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=5;
-ctx.arc(350,210,40,0,2*Math.PI);
-ctx.stroke();
+function show()
+{
+	var i= names_of_people.join("<br>");
+	console.log(names_of_people);
+	document.getElementById("p1").innerHTML=i.toString();
+	document.getElementById("sort_button").style.display="block";
+	
+}
 
-ctx=canvas.getContext("2d");
-color="red";
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=5;
-ctx.arc(450,210,40,0,2*Math.PI);
-ctx.stroke();
 
-ctx=canvas.getContext("2d");
-color="yellow";
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=5;
-ctx.arc(300,250,40,0,2*Math.PI);
-ctx.stroke();
+function sorting()
+	{
+		names_of_people.sort();        // add the sort function here
+		// .sort();
+		var i= names_of_people.join("<br>");
+		console.log(names_of_people);		
+		document.getElementById("sorted").innerHTML=i.toString();
+		}
 
-ctx=canvas.getContext("2d");
-color="green";
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=5;
-ctx.arc(400,250,40,0,2*Math.PI);
-ctx.stroke();
+
+function searching()
+{
+	var s= document.getElementById("s1").value;
+	var found=0;
+	var j;
+	for(j=0; j<names_of_people.length; j++)
+		{
+			if(s==names_of_people[j]){
+				found=found+1;
+			}	
+		}
+	document.getElementById("p2").innerHTML="name found "+found+" time/s";
+	console.log("found name "+found+" time/s");
+}
