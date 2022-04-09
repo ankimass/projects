b05574f824d4cc418893e12f65fd5456fcccc0fc
ankitha <94835759+ankimass@users.noreply.github.com>
2022-04-09 14:14:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,49 @@
-menu_list_array = ["Veg Margherita Pizza",
-                        "Chicken Tandoori Pizza",
-                        "Veg Supreme Pizza",
-                        "Paneer Tikka Pizza",
-                        "Deluxe Veggie Pizza",
-                        "Veg Extravaganza Pizza"];
-    
-    function getmenu(){
-        var htmldata="";
-        menu_list_array.sort();
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+ menu_list_array[i] + '<br>'
-        }
-        document.getElementById("display_menu").innerHTML = htmldata;
-       
-    }
+canvas=document.getElementById("mycanvas");
 
-    function add_item(){
-        var htmldata;
-		var imgtags='<img id="im1" src="pizzaImg.png">'
-        var item=document.getElementById("add_item").value;
-        menu_list_array.sort();
-     
-        htmldata="";
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
-        }
-         document.getElementById("display_addedmenu").innerHTML = htmldata;
-		
-    }
+ctx=canvas.getContext("2d");
+color="grey";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=1;
+ctx.rect(150,143,430,200,);
+ctx.stroke();
 
-function add_top(){
-	var item=document.getElementById("add_item").value;
-    menu_list_array.push(item);
-	add_item();
-}
+ctx=canvas.getContext("2d");
+color="blue";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=5;
+ctx.arc(250,210,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx=canvas.getContext("2d");
+color="black";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=5;
+ctx.arc(350,210,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx=canvas.getContext("2d");
+color="red";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=5;
+ctx.arc(450,210,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx=canvas.getContext("2d");
+color="yellow";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=5;
+ctx.arc(300,250,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx=canvas.getContext("2d");
+color="green";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=5;
+ctx.arc(400,250,40,0,2*Math.PI);
+ctx.stroke();
